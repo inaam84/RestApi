f11b49aa63749e515a33e793064d07aa17ba8909
Inaam <inaam.cs@gmail.com>
2023-05-20 01:15:39 +0100

    Testing the github actions by adding duplicate function code

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,16 +51,4 @@ class User extends Authenticatable
         return false;
     }
 
-    public function function2()
-    {
-        $use = 1;
-        do {
-            echo "The number is: $use <br>";
-            $use++;
-        } while ($use <= 5);
-        for ($use = 0; $use <= 100; $use += 10) {
-            echo "The number is: $use <br>";
-        }
-        return false;
-    }
 }
